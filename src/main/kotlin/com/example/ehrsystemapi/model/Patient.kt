8767fd84f0442247c0b96d84070be32aa489ef0a
Vadim Peretokin <vadim.peretokin@carasent.com>
2024-07-05 13:13:08 +0200

    Add Patient model back

--- src/main/kotlin/com/example/ehrsystemapi/model/Patient.kt
+++ src/main/kotlin/com/example/ehrsystemapi/model/Patient.kt
@@ -10,5 +10,18 @@ data class Patient(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
 
+    @Column(nullable = false)
+    var name: String,
 
+    @Column(nullable = false)
+    var dateOfBirth: LocalDate,
+
+    @Column(nullable = false)
+    var gender: String,
+
+    @Column(nullable = false)
+    var phoneNumber: String,
+
+    @Column(nullable = false)
+    var email: String
 )
